907f6a24577c9a51938623051242a0864623e39e
Tom LaStrange <tj@lastrange.com>
2025-12-05 17:36:36 -0500

    added status line

--- .config/nvim/lua/plugins/heirline.lua
+++ .config/nvim/lua/plugins/heirline.lua
@@ -86,10 +86,242 @@ return {
 
     local TabLine = { TabPages }
 
+    -- StatusLine: Cool minimal design with everforest colors
+
+    -- Setup colors from everforest
+    local colors = {
+      bg0 = utils.get_highlight("Normal").bg,
+      fg = utils.get_highlight("Normal").fg,
+      green = utils.get_highlight("String").fg,
+      yellow = utils.get_highlight("Number").fg,
+      red = utils.get_highlight("Error").fg,
+      blue = utils.get_highlight("Function").fg,
+      gray = utils.get_highlight("Comment").fg,
+      orange = utils.get_highlight("Constant").fg,
+    }
+
+    -- Vim Mode component with color blocks
+    local ViMode = {
+      init = function(self)
+        self.mode = vim.fn.mode(1)
+      end,
+      static = {
+        mode_names = {
+          n = "N",
+          no = "N",
+          nov = "N",
+          noV = "N",
+          ["no\22"] = "N",
+          niI = "N",
+          niR = "N",
+          niV = "N",
+          nt = "N",
+          v = "V",
+          vs = "V",
+          V = "V",
+          Vs = "V",
+          ["\22"] = "V",
+          ["\22s"] = "V",
+          s = "S",
+          S = "S",
+          ["\19"] = "S",
+          i = "I",
+          ic = "I",
+          ix = "I",
+          R = "R",
+          Rc = "R",
+          Rx = "R",
+          Rv = "R",
+          Rvc = "R",
+          Rvx = "R",
+          c = "C",
+          cv = "C",
+          r = ".",
+          rm = "M",
+          ["r?"] = "?",
+          ["!"] = "!",
+          t = "T",
+        },
+        mode_colors = {
+          n = "green",
+          i = "yellow",
+          v = "red",
+          V = "red",
+          ["\22"] = "red",
+          c = "blue",
+          s = "orange",
+          S = "orange",
+          ["\19"] = "orange",
+          R = "orange",
+          r = "orange",
+          ["!"] = "red",
+          t = "green",
+        },
+      },
+      provider = function(self)
+        return " %2(" .. self.mode_names[self.mode] .. "%) "
+      end,
+      hl = function(self)
+        local mode = self.mode:sub(1, 1)
+        return { fg = "bg0", bg = self.mode_colors[mode], bold = true }
+      end,
+      update = {
+        "ModeChanged",
+        pattern = "*:*",
+        callback = vim.schedule_wrap(function()
+          vim.cmd("redrawstatus")
+        end),
+      },
+    }
+
+    -- Git branch and changes
+    local Git = {
+      condition = conditions.is_git_repo,
+      init = function(self)
+        self.status_dict = vim.b.gitsigns_status_dict
+      end,
+      {
+        provider = function(self)
+          return "  " .. self.status_dict.head .. " "
+        end,
+        hl = { fg = "blue", bold = true },
+      },
+      {
+        condition = function(self)
+          return self.status_dict.added and self.status_dict.added > 0
+        end,
+        provider = function(self)
+          return "+" .. self.status_dict.added .. " "
+        end,
+        hl = { fg = "green" },
+      },
+      {
+        condition = function(self)
+          return self.status_dict.changed and self.status_dict.changed > 0
+        end,
+        provider = function(self)
+          return "~" .. self.status_dict.changed .. " "
+        end,
+        hl = { fg = "yellow" },
+      },
+      {
+        condition = function(self)
+          return self.status_dict.removed and self.status_dict.removed > 0
+        end,
+        provider = function(self)
+          return "-" .. self.status_dict.removed .. " "
+        end,
+        hl = { fg = "red" },
+      },
+    }
+
+    -- Filename with modified indicator
+    local FileName = {
+      init = function(self)
+        self.filename = vim.api.nvim_buf_get_name(0)
+      end,
+      {
+        provider = function(self)
+          local filename = vim.fn.fnamemodify(self.filename, ":t")
+          if filename == "" then
+            return "[No Name] "
+          end
+          return filename .. " "
+        end,
+        hl = { fg = "fg", bold = true },
+      },
+      {
+        condition = function()
+          return vim.bo.modified
+        end,
+        provider = "[+] ",
+        hl = { fg = "yellow", bold = true },
+      },
+    }
+
+    -- LSP Active indicator
+    local LSPActive = {
+      condition = conditions.lsp_attached,
+      update = { "LspAttach", "LspDetach" },
+      provider = " LSP ",
+      hl = { fg = "green", bold = true },
+    }
+
+    -- Diagnostics
+    local Diagnostics = {
+      condition = conditions.has_diagnostics,
+      static = {
+        error_icon = " ",
+        warn_icon = " ",
+        info_icon = " ",
+        hint_icon = " ",
+      },
+      init = function(self)
+        self.errors = #vim.diagnostic.get(0, { severity = vim.diagnostic.severity.ERROR })
+        self.warnings = #vim.diagnostic.get(0, { severity = vim.diagnostic.severity.WARN })
+        self.hints = #vim.diagnostic.get(0, { severity = vim.diagnostic.severity.HINT })
+        self.info = #vim.diagnostic.get(0, { severity = vim.diagnostic.severity.INFO })
+      end,
+      update = { "DiagnosticChanged", "BufEnter" },
+      {
+        condition = function(self)
+          return self.errors > 0
+        end,
+        provider = function(self)
+          return self.error_icon .. self.errors .. " "
+        end,
+        hl = { fg = "red" },
+      },
+      {
+        condition = function(self)
+          return self.warnings > 0
+        end,
+        provider = function(self)
+          return self.warn_icon .. self.warnings .. " "
+        end,
+        hl = { fg = "yellow" },
+      },
+    }
+
+    -- File position
+    local FilePosition = {
+      provider = " %l:%c ",
+      hl = { fg = "fg" },
+    }
+
+    -- File progress
+    local FileProgress = {
+      provider = " %P ",
+      hl = { fg = "gray" },
+    }
+
+    -- Spacer to push right-aligned content
+    local Spacer = { provider = "%=" }
+
+    -- Small spacer
+    local Space = { provider = " " }
+
+    -- Assemble statusline
+    local StatusLine = {
+      ViMode,
+      Space,
+      Git,
+      FileName,
+      Spacer,
+      LSPActive,
+      Diagnostics,
+      FilePosition,
+      FileProgress,
+    }
+
     require("heirline").setup({
+      statusline = StatusLine,
       tabline = TabLine,
+      opts = {
+        colors = colors,
+      },
     })
 
-    vim.o.showtabline = 2
+    vim.o.showtabline = 1  -- Only show tabline when there are 2+ tabs
   end
 }
