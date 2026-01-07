2abf66066d2ea31b72fa7738336c6fea40678c2c
Thomas LaStrange txl25 <tlastrange@cas.org>
2026-01-07 13:07:42 -0500

    fixed obsidian note names (nil error)

--- .config/nvim/lua/plugins/obsidian.lua
+++ .config/nvim/lua/plugins/obsidian.lua
@@ -53,22 +53,16 @@ return {
     ---@param title string|?
     ---@return string
     note_id_func = function(title)
-      -- Create note IDs in a Zettelkasten format with a timestamp and a suffix.
-      -- In this case a note with the title 'My new note' will be given an ID that looks
-      -- like '1657296016-my-new-note', and therefore the file name '1657296016-my-new-note.md'.
-      -- You may have as many periods in the note ID as you'd like—the ".md" will be added automatically
-      return title
+      return title or tostring(os.time())  -- fallback if nil
     end,
 
     -- Optional, customize how note file names are generated given the ID, target directory, and title.
     ---@param spec { id: string, dir: obsidian.Path, title: string|? }
     ---@return string|obsidian.Path The full path to the new note.
     note_path_func = function(spec)
-      -- This is equivalent to the default behavior.
-      local path = spec.dir / tostring(spec.title)
-      return path:with_suffix ".md"
+      local path = spec.dir / tostring(spec.id)
+      return path:with_suffix(".md")
     end,
-
     ---This was set to allow obsidian to be open in the background for obsidian sync.
     ---It likely makes not_id_func do nothing
     frontmatter = {
